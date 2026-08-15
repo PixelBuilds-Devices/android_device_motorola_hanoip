634d739bce59e601a0e4a2cbad5911d3135edbe4
DhineshCool <dhineshcool585@gmail.com>
2026-08-15 19:28:05 +0530

    Revert "hanoip: update build descriptors overrides"
    This reverts commit 44199d88f0ab5c59c6e1c3d8d7999ee6576a9075.

--- aosp_hanoip.mk
+++ aosp_hanoip.mk
@@ -43,9 +43,9 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Build info
+BUILD_FINGERPRINT := "motorola/hanoip_retail/hanoip:11/S2RI32.32-20-9-9-2/1591b:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="hanoip_retail-user 11 S2RI32.32-20-9-9-2 1591b release-keys" \
-    BuildFingerprint=motorola/hanoip_retail/hanoip:11/S2RI32.32-20-9-9-2/1591b:user/release-keys \
-    DeviceProduct=hanoip_retail
+    TARGET_PRODUCT=hanoip_retail \
+    PRIVATE_BUILD_DESC="hanoip_retail-user 11 S2RI32.32-20-9-9-2 1591b release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
